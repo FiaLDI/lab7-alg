825790a4a700eaf8985c0188f3c340c125df77cd
FiaLDI <leon.youtube2017@yandex.ru>
2023-10-23 09:22:00 +0300

    2

--- prog/proccedurhaffman.py
+++ prog/proccedurhaffman.py
@@ -1,3 +1,28 @@
+from readline import write_history_file
+
+
+k = -1
+
+def index(a):
+    global k
+    k += 1
+    return k
+        
+
+def coding(a):
+    t = set(a)
+    b = [[char, str(index(t))] for char in t]
+    cur = "0"
+    for i in range(len(b)):
+        if (b[i][1] == '0'):
+            continue
+        elif i == len(b)-1:
+            cur = '1' * len(cur)
+        else:
+            cur = '1' + cur
+        b[i][1] = cur
+    code = ""
+    
 
 
 def Extractmin(H):
@@ -29,5 +54,8 @@ def hoffman(frequencies):
 
 
 if __name__ == "__main__":
+    a = "abacabad"
+    b = "01001100100111"
+    coding(a)
     frequencies = [3, 5, 10, 21]
     print(hoffman(frequencies))
